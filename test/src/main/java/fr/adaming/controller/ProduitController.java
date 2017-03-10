e4aded48edec242c0de8e692244c5e4c81bd8746
inti0277 <inti0277@DESKTOP-QVPF7VB>
2017-03-10 19:51:07 +0100

    controller

---
+++ test/src/main/java/fr/adaming/controller/ProduitController.java
@@ -0,0 +1,10 @@
+package fr.adaming.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/produit")
+public class ProduitController {
+
+}
